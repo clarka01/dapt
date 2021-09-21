f65ce713f40765643e4a50c71b8931954fe06c83
Aaron Clark <78091625+clarka01@users.noreply.github.com>
2021-09-20 21:08:38 -0400

    restructure code to read from csv to save time. added csv to git ignore

--- costar/geographic_data/geo_analysis_AC.py
+++ costar/geographic_data/geo_analysis_AC.py
@@ -62,28 +62,6 @@ now = datetime.now()
 month = today_date.strftime("%B").upper()
 
 
-def query_costar(query):
-    ''' query from Costar Database'''
-
-    conn = pyodbc.connect('Driver={SQL Server};'
-                        'Server=DESKTOP-V8FUEV1\SQL12345;'
-                        'Database=Costar;'
-                        'Trusted_Connection=yes;')
-    cursor = conn.cursor()
-
-    df = pd.read_sql_query(query, conn)
-
-    return  df
-
-
-def merge_lease_sqlserv():
-    '''pull sql server tables into DFs and merge, drop cols'''
-
-    q1 = 'select * from dbo.merged_lease'
-    df1 = query_costar(q1)
-
-    return df
-
 def merge_lease_aws():
 #   '''connect to aws server'''
 
@@ -108,54 +86,72 @@ def merge_lease_aws():
 
 
 def load_data():
-    
-    try:
+    '''tries to read previous csv file to save pull time
+        otherwise, pulls from aws server'''
+
+    try:   
+        df = pd.read_csv('df_aws_merged.csv')
+
+    except:
+        def format_types():
+            '''format types for analysis/visuals; object to int, float, date'''
+            
+            df = merge_lease_aws()
+
+            #df.fillna('',inplace=True)
+
+            to_float1 = ['leasedeal_id', 'property_id', 'property_type_id', 
+                    'location_occupancy_id', 'service_type_id', 'sqft_min', 
+                    'sqft_max', 'renewal', 'actual_vacancy', 'rba', 'lease_term_inmonths',
+                    'free_months', 'cbsaid', 'buildingrating_id', 
+                    'construction_year', 'zip', 'days_on_market'] 
+                    #'building_age', #'lease_start_year'
+            
+            df[to_float1] = df[to_float1].applymap(float)
+            # df[to_int] = df[to_int].applymap(int)
+            '''see https://pandas.pydata.org/pandas-docs/stable/user_guide/gotchas.html#support-for-integer-na'''
+
+
+            to_float2 = ['estimated_rent', 'rate_actual', 'tenantimprovementallowancepersqft']
+            df[to_float2] = df[to_float2].applymap(float) 
+
+
+            to_date = ['date_on_market', 'date_off_market', 'lease_sign_date',
+                        'lease_expiration_date']
+            df[to_date] = df[to_date].apply(pd.to_datetime, errors = 'coerce')
+
+            # add col, lease_start year
+            df['lease_start_year'] = pd.DatetimeIndex(df['from_date']).year
+            # add col building_age
+            df['building_age'] = datetime.today().year - df['construction_year']
+
+            # remove submarket null values
+            df.dropna(subset = ['submarket_name']) #1,067,141 to 900,083
+
+            return df
+
         df = merge_lease_aws()
-    except: 
-        df = merge_lease_sqlserv()
 
     return df
+
+
+
+''' FUNCTION TO DATAFRAME......................'''
+
+def  csv_file_write():
+    '''writes to df (remember to adjust .gitignore to ignore this file) '''
+
+    df = load_data()
+    
+    df.to_csv('df_aws_merged.csv')
+
 
 #%%
 
-def format_types():
-    '''format types for analysis/visuals; object to int, float, date'''
-    
-    df = load_data()
-
-    #df.fillna('',inplace=True)
-
-    to_float1 = ['leasedeal_id', 'property_id', 'property_type_id', 
-            'location_occupancy_id', 'service_type_id', 'sqft_min', 
-            'sqft_max', 'renewal', 'actual_vacancy', 'rba', 'lease_term_inmonths',
-            'free_months', 'cbsaid', 'buildingrating_id', 
-            'construction_year', 'zip', 'days_on_market'] 
-            #'building_age', #'lease_start_year'
-    
-    df[to_float1] = df[to_float1].applymap(float)
-    # df[to_int] = df[to_int].applymap(int)
-    '''see https://pandas.pydata.org/pandas-docs/stable/user_guide/gotchas.html#support-for-integer-na'''
+'''DESCRIPTIVE STATS..................................'''
 
 
-    to_float2 = ['estimated_rent', 'rate_actual', 'tenantimprovementallowancepersqft']
-    df[to_float2] = df[to_float2].applymap(float) 
-
-
-    to_date = ['date_on_market', 'date_off_market', 'lease_sign_date',
-                'lease_expiration_date']
-    df[to_date] = df[to_date].apply(pd.to_datetime, errors = 'coerce')
-
-    # add col, lease_start year
-    df['lease_start_year'] = pd.DatetimeIndex(df['from_date']).year
-    # add col building_age
-    df['building_age'] = datetime.today().year - df['construction_year']
-
-    return df
-
-df = format_types()
-
-
-# PERCENT NULLS
+PERCENT NULLS
 def pct_null():
     '''% of null values for pca analysis/general needs'''
     
@@ -163,21 +159,22 @@ def pct_null():
     print(null_pct)
 
 
-#%%
-
-'''DESCRIPTIVE STATS..................................'''
-
 # narrow columns to variables of interest
-num_cols = ['lease_term_inmonths','free_months', 
-            'buildingrating_id', 'sqft_min', 'sqft_max', 
-            'construction_year', 'days_on_market', 
-            'lease_start_year', 'building_age', 'lease_expiration_date']
 
-df_main = df[num_cols]
+def desc_stats():
 
-# produce stats for outlier detection
-pcts = [.001,.01, .025, .25, .5, .75, .975, .99, .999]
-df_desc = df_main.describe(percentiles= pcts).T
+    num_cols = ['lease_term_inmonths','free_months', 
+                'buildingrating_id', 'sqft_min', 'sqft_max', 
+                'construction_year', 'days_on_market', 
+                'lease_start_year', 'building_age', 'lease_expiration_date']
+
+    df_main = df[num_cols]
+
+    # produce stats for outlier detection
+    pcts = [.001,.01, .025, .25, .5, .75, .975, .99, .999]
+    df_desc = df_main.describe(percentiles= pcts).T
+
+    return df_desc
 
 # df_desc.to_csv('data_set_stats.csv')
 
@@ -185,13 +182,11 @@ df_desc = df_main.describe(percentiles= pcts).T
 #%%
 
 
-'''HISTOGRAMS FOR KEY DATA & FEATURE ENGINEERING.............'''
+'''HISTOGRAMS FOR KEY DATA COLUMNS & FEATURE ENGINEERING.............'''
 
 '''General Cleaning...'''
 
-# remove submarkets without meaning
 
-df.dropna(subset = ['submarket_name']) #1,067,141 to 900,083
 
 #%%
 
@@ -202,7 +197,10 @@ df.dropna(subset = ['submarket_name']) #1,067,141 to 900,083
 df =  df.loc[~(df.lease_term_inmonths <= 0)] #900,083 to #893,993
 
 # histogram
+min_term = df.lease_term_inmonths.min()
 max_term = df.lease_term_inmonths.max()
+
+
 df.lease_term_inmonths.hist(bins = 100, 
                             figsize = (12, 8), 
                             color = 'green', 
